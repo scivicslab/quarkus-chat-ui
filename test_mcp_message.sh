8f9981eac9b8c9b5e344fdaedef8a222b0028289
scivicslab <devteam@scivics-lab.com>
2026-04-05 12:03:03 +0900

    chore: update .gitignore to exclude session files and test artifacts

--- test_mcp_message.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Test sending message to port 8203 chat API
-
-curl -X POST http://localhost:8203/api/chat \
-  -H "Content-Type: application/json" \
-  -d '{"text": "Hello from MCP test - testing message delivery"}' \
-  -w "\n"
